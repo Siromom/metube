37fa777dfb9809a1ae2f22925f05ff18ad7d049d
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-02 17:19:41 +0900

    Add partials, locals middleware

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from './middlewares';
 const app = express();
+
+app.use(helmet());
 
 app.set('view engine', 'pug');
 
 app.use(morgan("dev"));
-app.use(helmet());
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.home, userRouter);
