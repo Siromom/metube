82e5951ebee9347f80a62373260720d8f05e22de
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-03 23:06:21 +0900

    Iteration

---
+++ db.js
@@ -0,0 +1,51 @@
+//fake data
+export const videos = [
+    {
+        id: 3141592,
+        title: 'Video awesome',
+        description: 'This is description',
+        views: 92,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 12777,
+            name: "SeWon",
+            email: "se@won.com"
+        }
+    },
+    {
+        id: 3141593,
+        title: 'Video super',
+        description: 'This is description',
+        views: 92,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 12777,
+            name: "SeWon",
+            email: "se@won.com"
+        }
+    },
+    {
+        id: 3141594,
+        title: 'Video nice',
+        description: 'This is description',
+        views: 92,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 12777,
+            name: "SeWon",
+            email: "se@won.com"
+        }
+    },
+    {
+        id: 3141555,
+        title: 'Video perfect',
+        description: 'This is description',
+        views: 92,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 12777,
+            name: "SeWon",
+            email: "se@won.com"
+        }
+    }
+]
