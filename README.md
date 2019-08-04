6d997354d158857914104070ac810501e1f07678
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-04 21:59:54 +0900

    Add Join, Login, Logout, ,UserDetial, Upload, VideoDetail

--- README.md
+++ README.md
@@ -3,16 +3,3 @@
 Cloning Youtube with Vanilla and NodeJS
 
 
-## Pages:
-
-- [x] Home
-- [x] Join
-- [x] Login
-- [x] Search
-- [ ] User Detail
-- [x] Edit Profile
-- [ ] Change Password
-- [ ] Upload
-- [ ] Video Detail
-- [ ] Edit Video
-
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import routes from '../routes';
 import { home, serch } from '../controllers/videoController';
-import { getJoin, postJoin, login, logout } from '../controllers/userController';
+import { getJoin, postJoin, getLogin, postLogin, logout } from '../controllers/userController';
 
 const globalRouter = express.Router();
 
@@ -10,7 +10,9 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, serch);
 
