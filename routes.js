f6c4a87bd41846b0969c92b0408dc493d57ec0fb
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-07 15:14:34 +0900

    Add editVideo

--- routes.js
+++ routes.js
@@ -40,13 +40,20 @@ const routes = {
     upload: UPLOAD,
     videoDetail: (id) => {
         if(id){
-            return `${id}`;
+            return `/videos/${id}`;
         }
         else {
             return VIDEO_DETAIL;
         }
     },
-    editVideo: EDIT_VIDEO,
+    editVideo: (id) => {
+        if(id){
+            return `/videos/${id}/edit`;
+        }
+        else {
+            return EDIT_VIDEO;
+        }
+    },
     deleteVideo: DELETE_VIDEO
 };
 
