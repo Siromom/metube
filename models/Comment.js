09ba31f1167e80cea3430843d961a103d8043012
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-08-09 15:48:44 +0900

    Add comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: 'Text is required'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    video: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Video"
-    }
-})
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const model = mongoose.model("Comment", CommentSchema);
 export default model;
