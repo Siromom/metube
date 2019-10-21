48317ee24c4f86354335b43d625a8f1624acde80
SeWonKim <30452963+Siromom@users.noreply.github.com>
2019-10-21 21:06:40 +0900

    [ADD] Prestart

---
+++ src/init.js
@@ -0,0 +1,19 @@
+import "@babel/polyfill";
+import dotenv from "dotenv";
+
+import "./db";
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
+
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
